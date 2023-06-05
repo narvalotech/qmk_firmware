16b90dfd482e855a113dee5bbea4eaaa95256877
Jonathan Rico <jonathan@rico.live>
2023-06-05 07:52:44 +0200

    add reboot key

--- keyboards/nv0/keymaps/default/keymap.c
+++ keyboards/nv0/keymaps/default/keymap.c
@@ -17,7 +17,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_UPPER] = LAYOUT(
-  ____,   ____,   ____,    ____,    ____,    ____,             ____,    ____,    ____,  KC_LBRC, KC_RBRC,  ____,
+  QK_BOOT,QK_RBT, ____,    ____,    ____,    ____,             ____,    ____,    ____,  KC_LBRC, KC_RBRC,  ____,
   KC_F1,  KC_F2,  KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11,   KC_F12,
   KC_ESC, ____,   ____,    ____,    ____,    ____,             KC_INS,  ____,    ____,  ____,    ____,     ____,
   ____,   ____,   ____,    ____,    ____,    ____,             KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, ____,     ____,
